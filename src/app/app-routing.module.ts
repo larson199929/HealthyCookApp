c4c4ada79f8da416ddce4d7144747371b6dbcaee
larson199929 <63831897+larson199929@users.noreply.github.com>
2021-07-01 03:29:44 -0500

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,17 +10,17 @@ import { UpdateBeginnerComponent } from './update-beginner/update-beginner.compo
 import { UpdateRecipeComponent } from './update-recipe/update-recipe.component';
 import { CreateChefComponent } from './create-chef/create-chef.component';
 import { FindChefComponent } from './find-chef/find-chef.component';
-import { ListChefComponent } from './list-chef/list-chef.component'
+import { ListChefComponent } from './list-chef/list-chef.component';
 import { UpdateChefComponent } from './update-chef/update-chef.component';
 import { FindByIngredientComponent } from './find-by-ingredient/find-by-ingredient.component';
 import { RecipeZanahoriaComponent } from './recipe-zanahoria/recipe-zanahoria.component';
 
 const routes: Routes = [
   {path: '', redirectTo:'beginner', pathMatch:'full'},
-  {path: 'beginner/listar', component: ListBeginnerComponent},
-  {path: 'beginner/nuevo', component:CreateBeginnerComponent},
-  {path: 'beginner/actualizar/:id', component: UpdateBeginnerComponent},
-  {path: 'beginner/buscar', component: FindBeginnerComponent},
+  {path: 'listar', component: ListBeginnerComponent},
+  {path: 'nuevo', component:CreateBeginnerComponent},
+  {path: 'actualizar/:id', component: UpdateBeginnerComponent},
+  {path: 'buscar', component: FindBeginnerComponent},
 
   {path: '',redirectTo:'recipe', pathMatch:'full'},
   {path:'recipe/listar', component: ListRecipeComponent},
